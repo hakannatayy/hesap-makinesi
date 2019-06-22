e1fe92678773649e17689678021e107f5146d0ee
hakannatayy <48553376+hakannatayy@users.noreply.github.com>
2019-06-22 17:37:47 +0300

    işlemler belrilendi.

--- Hesapmakinasıortadüzey.py
+++ Hesapmakinasıortadüzey.py
@@ -1,1 +1,17 @@
 
+print("""
+[1] Toplama İşlemi
+[2] Çıkarma İşlemi
+[3] Çarpma İşlemi
+[4] Bölme İşlemi
+[q]Çıkış
+
+
+
+""")
+
+giris = input("Seçiminiz: ")
+
+if giris=="1":
+    x =input("İlk Sayı: ")
+    x =float(x)
